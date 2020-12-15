60e611c75a0c39a7a903fdc78ae11118e6e871f3
justiceHui <jhnah917@naver.com>
2020-12-15 22:13:37 +0900

    fix /boj/tag router

--- src/routers/boj/boj.router.js
+++ src/routers/boj/boj.router.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 router.get(/\/user\/(.+)/, BOJ.getUser);
 router.get(/\/random_problem\/(.+)/, BOJ.getRandomProblem);
 router.get(/\/search_problem\/(.+)/, BOJ.searchProblem);
-router.get(/\/tag\/(.+)/, BOJ.getProblemTag);
+router.get(/\/tag\/(\d+)/, BOJ.getProblemTag);
 router.get('/', (req, res) => {
     res.send('boj');
 });
